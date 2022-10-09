56c74794f7d6534f3e13d29995750a44bd48d12c
YoelCastilloo99 <jecm1999jcastillo@gmail.com>
2022-10-09 00:19:31 -0600

    Modificacion prueba

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -151,7 +151,7 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Gay el que lo lea</h1>
                         <!--<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>-->
                     </div>
@@ -251,7 +251,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright Return APP 2022</span>
+                        <span>Copyright Grupo 5 IGSS 2022</span>
                     </div>
                 </div>
             </footer>
